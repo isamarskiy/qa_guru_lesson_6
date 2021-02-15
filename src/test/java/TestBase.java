1139a3602f099daf7d6d4ac9db1394a5949c4e0f
sergey <zendmir5@yandex.ru>
2021-02-15 21:46:32 +0300

    added selenide search test

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,9 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    public static void setUp() {
+        Configuration.browserSize="1920x1080";
+    }
+}

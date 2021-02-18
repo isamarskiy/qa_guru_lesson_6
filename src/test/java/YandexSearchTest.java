af558333686ed9c61982f90fc6abbfdb5fd1c096
samarskiy <samarskyii@gmail.com>
2021-02-18 23:39:45 +0300

    (fix)new test: search selenide in yandex

--- src/test/java/YandexSearchTest.java
+++ src/test/java/YandexSearchTest.java
@@ -8,8 +8,10 @@ public class YandexSearchTest {
 
     @Test
     void searchYandex() {
+        String search_word = "selenide";
+
         open("https://ya.ru");
-        $("#text").setValue("selenide").pressEnter();
-        $("#search-result").shouldHave(text("selenide"));
+        $("#text").setValue(search_word).pressEnter();
+        $("#search-result").shouldHave(text(search_word));
     }
 }
